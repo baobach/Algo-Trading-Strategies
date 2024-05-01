5d2c1591435e3ed2eea548724fe77eaaed7a0961
baobach <95854748+baobach@users.noreply.github.com>
2024-05-01 15:14:14 +0700

    Refactor EMA_BUY strategy to use order_target_percent instead of buy and close functions

--- backtest/run.py
+++ backtest/run.py
@@ -3,26 +3,27 @@ import sys
 import backtrader as bt
 import pandas as pd
 from util.analyzer import AnalyzerSuite
-from strategies.LongOnly import *
+from strategies.EMA_BUY import *
+import quantstats
 
-strategy = Long4
+strategy = EMA_BUY
 
-dataname = "./data/raw/BTCUSDT_1hour.csv"
+dataname = "./data/raw/5EMA_5BN.csv"
 
 # Create a cerebro entity
 cerebro = bt.Cerebro()
 data = bt.feeds.GenericCSVData(
     dataname=dataname,
-    fromdate = pd.Timestamp('2021-01-01'),
-    todate = pd.Timestamp('2021-05-05'),
+    # fromdate = pd.Timestamp('2021-01-01'),
+    # todate = pd.Timestamp('2021-05-05'),
     # Check column names for the data feed
     datetime=0,
     open=1,
     high=2,
     low=3,
     close=4,
-    volume=6,
-    openinterest=5,
+    volume=-1,
+    openinterest=-1,
 )
 
 # Set the colors of the bar plot
@@ -47,14 +48,24 @@ cerebro.broker.setcash(100_000.0)
 # Print out the starting conditions
 print("Starting Portfolio Value: %.2f" % cerebro.broker.getvalue())
 # Analyzer
-AnalyzerSuite.defineAnalyzers(AnalyzerSuite, cerebro)
+# AnalyzerSuite.defineAnalyzers(AnalyzerSuite, cerebro)
+cerebro.addanalyzer(bt.analyzers.PyFolio, _name='PyFolio')
+
 # Run over everything
 thestrats = cerebro.run()
+strat = thestrats[0]
 
 # -----------------------------------------------------------------------------------
 
-print(AnalyzerSuite.returnAnalyzers(AnalyzerSuite, thestrats))
-# Print out the final result
-print("Final Portfolio Value: %.2f" % cerebro.broker.getvalue())
-# Plot the result
-cerebro.plot()
+# print(AnalyzerSuite.returnAnalyzers(AnalyzerSuite, thestrats))
+# # Print out the final result
+# print("Final Portfolio Value: %.2f" % cerebro.broker.getvalue())
+# # Plot the result
+# cerebro.plot()
+
+# -----------------------------------------------------------------------------------
+# Quantstats
+portfolio_stats = strat.analyzers.getbyname('PyFolio')
+returns, positions, transactions, gross_lev = portfolio_stats.get_pf_items()
+returns.index = returns.index.tz_convert(None)
+quantstats.reports.html(returns, output='bn_strategy.html', title='BN Sentiment')
--- backtest/util/analyzer.py
+++ backtest/util/analyzer.py
@@ -1,16 +1,21 @@
 import backtrader.analyzers as btanalyzers
 
+riskfreerate = 0.04616
+
 class AnalyzerSuite():
     def defineAnalyzers(self, cerebro):
         cerebro.addanalyzer(btanalyzers.DrawDown, _name='mydrawdown')
-        cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='mysharpe')
+        cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='mysharpe', riskfreerate = riskfreerate)
+        cerebro.addanalyzer(btanalyzers.SortinoRatio, _name='mysortino', riskfreerate = riskfreerate)
         cerebro.addanalyzer(btanalyzers.Returns, _name='myreturn')
+        cerebro.addanalyzer(btanalyzers.TradeAnalyzer, _name='mytradeanalyzer')
 
     def returnAnalyzers(self, thestrats):
         thestrat = thestrats[0]
-        sharpe_ratio = thestrat.analyzers.mysharpe.get_analysis().get('sharperatio')
-        if sharpe_ratio is None:
-            sharpe_ratio = 0
-        return {'Maximum DrawDown: %.2f%%' % thestrat.analyzers.mydrawdown.get_analysis()['max']['drawdown'],
-                'Sharpe Ratio: %.2f' % sharpe_ratio,
-                'Annualized return: %.2f%%' % thestrat.analyzers.myreturn.get_analysis()['rnorm100']}
+        return {
+            'Maximum DrawDown %': round(thestrat.analyzers.mydrawdown.get_analysis()['max']['drawdown'], 4),
+            'Sharpe Ratio:': round(thestrat.analyzers.mysharpe.get_analysis()['sharperatio'], 4),
+            'Sortino Ratio': round(thestrat.analyzers.mysortino.get_analysis()['sortinoratio'], 4),
+            'Annualized return %:': round(thestrat.analyzers.myreturn.get_analysis()['rnorm100'], 4),
+            'Average holding bars:': round(thestrat.analyzers.mytradeanalyzer.get_analysis()['len']['average'], 4),
+        }
